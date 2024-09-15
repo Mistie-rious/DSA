f917649aed1b5220f9a7b62664e1819a96745774
Mistie-rious <misturayahaya1@gmail.com>
2024-09-15 13:49:48 +0100

    more stacks

--- stack.py
+++ stack.py
@@ -43,5 +43,44 @@ misty.push(2)
 misty.push(3)
 misty.push(4)
 misty.push(5)
-misty.push(6)
+
 print(misty.pop(), misty.size(), misty.peek(), misty.isFull())
+
+
+pythonList = []
+pythonList.append(1)
+pythonList.append("Cute")
+pythonList.append(3)
+pythonList.append(4.98)
+pythonList.append('Orange')
+print(pythonList.pop(), pythonList.pop(), pythonList.pop(), pythonList.pop())
+print(pythonList)
+
+from collections import deque
+
+misty = deque()
+misty.append(1)
+misty.append(2)
+misty.append('Great')
+print(misty.pop(), misty.pop(), misty.pop())
+
+from queue import LifoQueue
+
+mistylifo = LifoQueue(maxsize=5)
+
+print(mistylifo.full())
+
+mistylifo.put(3)
+mistylifo.put(6)
+mistylifo.put('popop')
+print(mistylifo.qsize())
+mistylifo.put(9)
+mistylifo.put(12)
+
+
+print(mistylifo.full())
+print(mistylifo.empty())
+print(mistylifo.get(), mistylifo.get(), mistylifo.get(), mistylifo.get(), mistylifo.get())
+print(mistylifo.empty())
+
+
